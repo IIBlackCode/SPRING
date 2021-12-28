54addf0b32504991fc696828ba154aff79355701
김민서 <blackcode-@naver.com>
2021-12-29 00:20:10 +0900

    238 PAGE CHAPTER 07 예외처리

---
+++ EX 01 [MariaDB Ver 2.0]/src/main/java/org/zerock/Controller/CommonExceptionAdvice.java
@@ -0,0 +1,28 @@
+package org.zerock.Controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+@ControllerAdvice // >@Controller로 선언하는 경우 404대신 에러 발생
+public class CommonExceptionAdvice {
+	
+	private static final Logger logger = LoggerFactory.getLogger(CommonExceptionAdvice.class);
+	
+//	@ExceptionHandler(Exception.class)
+//	public String common(Exception e) {
+//		logger.info(e.toString());
+//		return "error_common";
+//	}// The end of method
+	
+	@ExceptionHandler(Exception.class)
+	private ModelAndView errorModelAndView(Exception ex) {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("/error_common");
+		modelAndView.addObject("exception", ex);
+		return modelAndView;
+	}// The end of method
+	
+}// The end of class
