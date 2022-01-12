abcb4e157f1457f5daf4eb66c96619bd111be2de
김민서 <blackcode-@naver.com>
2022-01-13 01:38:14 +0900

    344 PAGE CHAPTER 10 검색처리와 동적 SQL

--- EX 01 [MariaDB Ver 2.0]/src/main/java/org/zerock/Controller/SearchBoardController.java
+++ EX 01 [MariaDB Ver 2.0]/src/main/java/org/zerock/Controller/SearchBoardController.java
@@ -1,6 +1,7 @@
 package org.zerock.Controller;
 
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,6 +10,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.domain.BoardVO;
+import org.zerock.domain.Criteria;
 import org.zerock.domain.PageMaker;
 import org.zerock.domain.SearchCriteria;
 import org.zerock.service.BoardService;
@@ -39,4 +44,83 @@ public class SearchBoardController {
 		model.addAttribute("pageMaker", pageMaker);
 		
 	}// The end of Method
+	
+	// 게시글 읽기
+	@RequestMapping(value = "/readPage", method = RequestMethod.GET)
+	public void read(@RequestParam("bno") int bno, @ModelAttribute("cri") SearchCriteria cri,Model model) throws Exception{
+		
+		logger.info("[BOARD] _ /read GET : ",bno);
+		
+		model.addAttribute(service.read(bno));
+		
+	}// The end of method
+	
+	// 게시글 삭제
+	@RequestMapping(value = "/removePage", method = RequestMethod.POST)
+	public String remove(@RequestParam("bno") int bno, SearchCriteria cri, RedirectAttributes rttr) throws Exception{
+		
+		logger.info("[BOARD] _ /remove POST : ",bno);
+		
+		service.remove(bno);
+		
+		rttr.addAttribute("page", cri.getPage());
+		rttr.addAttribute("perPageNum",cri.getPerPageNum());
+		rttr.addAttribute("searchType",cri.getSearchType());
+		rttr.addAttribute("keyword",cri.getKeyword());
+		
+		rttr.addFlashAttribute("msg", "SUCCESS");
+		
+		return "redirect:/sboard/list";
+	}// The end of method
+	
+	// 게시글 수정페이지 읽기
+	@RequestMapping(value = "/modifyPage", method = RequestMethod.GET)
+	public void modifyPagingGET(int bno, @ModelAttribute("cri") SearchCriteria cri, Model model) throws Exception{
+		
+		logger.info("[BOARD] _ /modifyPage GET : ",bno);
+		
+		model.addAttribute(service.read(bno));
+	}// The end of method
+	
+	// 게시글 수정
+	@RequestMapping(value = "/modifyPage", method = RequestMethod.POST)
+	public String modifyPagingPOST(BoardVO board, SearchCriteria cri, RedirectAttributes rttr) throws Exception{
+		
+		logger.info("[BOARD] _ /modifyPage POST : ",cri.toString());
+		service.modify(board);
+		
+		rttr.addAttribute("page", cri.getPage());
+		rttr.addAttribute("perPageNum",cri.getPerPageNum());
+		rttr.addAttribute("searchType",cri.getSearchType());
+		rttr.addAttribute("keyword",cri.getKeyword());
+		rttr.addFlashAttribute("msg", "SUCCESS");
+		
+		logger.info(rttr.toString());
+		
+		return "redirect:/sboard/list";
+	}// The end of method
+	
+	// 게시글 등록 GET
+	@RequestMapping(value = "/register", method = RequestMethod.GET)
+	public void registerGET() throws Exception{
+		
+		logger.info("[BOARD] _ /register GET");
+		
+	}// The end of method
+	
+	
+	// 게시글 등록 POST
+	@RequestMapping(value = "/register", method = {RequestMethod.POST})
+	public String registerPOST(BoardVO board, RedirectAttributes rttr) throws Exception{
+		
+		logger.info("[BOARD] _ /register POST");
+		logger.info("board : "+board.toString());
+		
+		service.regist(board);
+		
+		rttr.addFlashAttribute("msg", "success");
+//		return "/board/success";
+		return "redirect:/sboard/list";
+		
+	}// The end of method
 }
---
+++ EX 01 [MariaDB Ver 2.0]/src/main/webapp/WEB-INF/views/sboard/readPage.jsp
@@ -0,0 +1,89 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ include file="../include/header.jsp" %>
+
+<!-- Main Content -->
+<section class="content">
+	<div class="row">
+		<!-- left column -->
+		<div class="col-md-12">
+			<!-- general from elements -->
+			<div class="box">
+				<div class="box-header with-border">
+					<h3 class="box-title">Read Board</h3>
+				</div>
+				
+				<form role="boardReadform" method="post">
+					<input type="hidden" name="bno" value="${boardVO.bno}">
+				</form>
+				
+				<div class="box-body">
+					<div class="form-group">
+						<label for="exampleInputEmail1">BNO - [ ${boardVO.ip} ] </label>
+						<input type="text" name="title" class="form-control" value="${boardVO.bno}" readonly="readonly">
+					</div>
+				</div>
+				
+				<div class="box-body">
+					<div class="form-group">
+						<label for="exampleInputEmail1">Title</label>
+						<input type="text" name="title" class="form-control" value="${boardVO.title}" readonly="readonly">
+					</div>
+				</div>
+				
+				<div class="box-body">
+					<div class="form-group">
+						<label for="exampleInputPassword1">Content</label>
+						<%-- <input type="text" name="title" class="form-control" value="${boardVO.content}" readonly="readonly"> --%>
+						<%-- <textarea class="form-control" name="content" rows="3" readonly="readonly">${boardVO.content}</textarea> --%>
+						<div class="form-control" style= "height:auto;" name="summernote" readonly="readonly">${boardVO.content}</div>
+					</div>
+				</div>
+				
+				<div class="box-body">
+					<div class="form-group">
+						<label for="exampleInputEmail1">Writer</label>
+						<input type="text" name="writer" class="form-control" value="${boardVO.writer}" readonly="readonly">
+					</div>
+					<table class="form-group">
+						<tr>
+							<td><h4>지역 : </h4></td>
+							<td><h3 id="region_name"></td>
+						</tr>
+						<tr>
+							<td><h4>국가 : </h4></td>
+							<td><h3 id="country_name"></td>
+						</tr>
+						<tr>
+							<th><img id="country_flag"/></th>
+						</tr>
+					</table>
+				</div>
+				
+				<!-- /.box-body -->
+
+				<div class="box-footer">
+					<button type="submit" class="btn btn-warning modifyBtn">Modify</button>
+					<button type="submit" class="btn btn-danger removeBtn">REMOVE</button>
+					<button type="submit" class="btn btn-primary goListBtn">GO LIST</button>
+				</div>
+				
+			</div>
+		</div>
+	</div>
+</section>
+
+<form role="form" action="modifyPage" method="post">
+	<input type="hidden" name="bno" value="${boardVO.bno}">
+	<input type="hidden" name="page" value="${cri.page}">
+	<input type="hidden" name="perPageNum" value="${cri.perPageNum}">
+	<input type="hidden" name="searchType" value="${cri.searchType}">
+	<input type="hidden" name="keyword" value="${cri.keyword}">
+</form>
+
+<%@ include file="../include/script/Summernote.jsp" %>
+<%@ include file="../include/script/BoardReadPage.jsp" %>
+<%@ include file="../include/footer.jsp" %>
+
+
+
+
