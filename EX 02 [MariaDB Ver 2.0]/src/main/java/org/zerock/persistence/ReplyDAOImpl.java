4dbff871258ed594145393f2cf2774f91933749b
김민서 <blackcode-@naver.com>
2022-02-01 00:00:25 +0900

    [PAGE:376] PART3 CHAPTER 02 댓글처리와 REST [완료]

---
+++ EX 02 [MariaDB Ver 2.0]/src/main/java/org/zerock/persistence/ReplyDAOImpl.java
@@ -0,0 +1,41 @@
+package org.zerock.persistence;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.zerock.domain.ReplyVO;
+
+public class ReplyDAOImpl implements ReplyDAO {
+	
+	@Inject
+	private SqlSession session;
+	
+	private static String namespace = "org.zerock.mapper.ReplyMapper";
+
+	@Override
+	public List<ReplyVO> list(Integer bno) throws Exception {
+		// TODO Auto-generated method stub
+		return session.selectList(namespace + ".list", bno);
+	}
+
+	@Override
+	public void create(ReplyVO vo) throws Exception {
+		// TODO Auto-generated method stub
+		session.insert(namespace + ".create", vo);
+	}
+
+	@Override
+	public void update(ReplyVO vo) throws Exception {
+		// TODO Auto-generated method stub
+		session.insert(namespace + ".update", vo);
+	}
+
+	@Override
+	public void delete(Integer rno) throws Exception {
+		// TODO Auto-generated method stub
+		session.insert(namespace + ".delete", rno);
+	}
+
+}
